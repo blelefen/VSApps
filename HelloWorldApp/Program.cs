ee182aba2317fc5c22ba18fdace978e4c22de6cb
Artem <blelefen@gmail.com>
2023-02-18 17:48:13 +0300

    Добавьте файлы проекта.

---
+++ HelloWorldApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Hello, World!!!";
+            Console.WriteLine($"{a}");
+            Console.WriteLine("Hello, World!!!");
+            Console.WriteLine("{0}",a);
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
