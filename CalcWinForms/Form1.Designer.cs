8c42d0cc407af4cea8e2062d686877a53091380b
Artem <blelefen@gmail.com>
2023-04-16 11:33:16 +0300

    Создал калькулятор на WinForms

---
+++ CalcWinForms/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace CalcWinForms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 350);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(510, 99);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 106);
+            this.textBox1.MaximumSize = new System.Drawing.Size(200, 50);
+            this.textBox1.MaxLength = 7;
+            this.textBox1.MinimumSize = new System.Drawing.Size(200, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 50);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(370, 106);
+            this.textBox2.MaximumSize = new System.Drawing.Size(200, 50);
+            this.textBox2.MaxLength = 7;
+            this.textBox2.MinimumSize = new System.Drawing.Size(200, 50);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(200, 50);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(189, 256);
+            this.textBox3.MaximumSize = new System.Drawing.Size(200, 50);
+            this.textBox3.MaxLength = 7;
+            this.textBox3.MinimumSize = new System.Drawing.Size(200, 50);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(200, 50);
+            this.textBox3.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.comboBox1.Location = new System.Drawing.Point(229, 121);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(123, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(342, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Введите числа и выберите необходимое действие";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(582, 553);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainForm";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CalcWinForms/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalcWinForms
+{
+    public partial class MainForm : Form
+    {
+        
+
+        public MainForm()
+        {
+
+            InitializeComponent();
+
+            
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int one = Convert.ToInt32(textBox1.Text);
+            int two = Convert.ToInt32(textBox2.Text);
+            char and = Convert.ToChar(comboBox1.Text);
+
+            double res = resClick(one,and,two);
+            textBox3.Text = Convert.ToString(res);
+        }
+
+        public double resClick(int a,char b,int c)
+        {
+            double r=0;
+            switch(b)
+
+            { 
+                case '+':
+                    r = a + c;
+                    break;
+                case '-':
+                    r = a - c;
+                    break;
+                case '*':
+                    r = a * c;
+                    break;
+                case '/':
+                    r = (double)a / c;
+                    break;
+
+            }
+            return r;
+        }
+
+        
+    }
+}
